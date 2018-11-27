02073486395b5899216a076ec9b65b410a0948c0
Allen <39018805+wangcxallen@users.noreply.github.com>
2018-11-27 01:02:08 -0500

    Add files via upload

--- tx.py
+++ tx.py
@@ -6,5 +6,5 @@
 
 import subprocess, shlex
 cmd = shlex.split("/home/pi/dw1000_rpi_new/src/dw1000_tx")
-print(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode("utf-8"))
-
+p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+print(p.communicate()[0].decode("utf-8"))
