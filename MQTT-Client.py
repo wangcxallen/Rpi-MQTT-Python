9455ee832f5b7109c11935e9203d74571ba31c01
Allen <39018805+wangcxallen@users.noreply.github.com>
2018-11-27 01:05:29 -0500

    Add files via upload

--- MQTT-Client.py
+++ MQTT-Client.py
@@ -20,11 +20,13 @@ def UWB_on_Message(client, userdata, msg):
         if(speaker== FLAG):
             print("say")
             cmd = shlex.split("/home/pi/dw1000_rpi_new/src/dw1000_tx")
-            print(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode("utf-8"))
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            print(p.communicate()[0].decode("utf-8"))
         else:
             print("hear")
             cmd = shlex.split("/home/pi/dw1000_rpi_new/src/dw1000_rx_cir")
-            print(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode("utf-8"))
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            print(p.communicate()[0].decode("utf-8"))
     
 def on_connect(client, userdata, flags, rc):
     print("Connection returned result: " + connack_string(rc))
